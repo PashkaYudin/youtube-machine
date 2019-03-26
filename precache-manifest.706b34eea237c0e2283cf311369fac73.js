57d60fd8bd7ec91bb82aa700a8ed68285867124d
root <root@PASHKIN.localdomain>
2019-03-26 18:05:58 +0700

    first commit

---
+++ precache-manifest.706b34eea237c0e2283cf311369fac73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4aed817b779c75d9a0b",
+    "url": "/static/js/main.8a29bb96.chunk.js"
+  },
+  {
+    "revision": "c411c6295b691d07baae",
+    "url": "/static/js/2.500d5162.chunk.js"
+  },
+  {
+    "revision": "b4aed817b779c75d9a0b",
+    "url": "/static/css/main.4fabdb83.chunk.css"
+  },
+  {
+    "revision": "684ec95fd6026b6af6f4e7b37c41c567",
+    "url": "/index.html"
+  }
+];
